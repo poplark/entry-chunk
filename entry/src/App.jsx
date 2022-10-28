aa816a07bf962b645163b1dd46344528c1450e65
poplark <songkang666@126.com>
2022-10-28 13:07:21 +0800

    feat: entry - loadWidgets

--- entry/src/App.jsx
+++ entry/src/App.jsx
@@ -1,19 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect, Suspense, lazy } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function Dashboard() {
+  return <div>
+    <p>Dashboard</p>
+    todo - content - widget - router
+  </div>
+}
+function About() {
+  return <div>
+    <p>About</p>
+    todo - content - widget - router
+  </div>
+}
+
+function loadWidgets() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        {name: 'xx', path: '/xx', file: '/widgets/xxx'},
+        {name: 'yy', path: '/yy', file: '/widgets/yyy'},
+      ])
+    }, 1000);
+  });
+}
 
 export function App() {
+  const [widgets, setWidgets] = useState([]);
+  useEffect(() => {
+    loadWidgets().then((widgets) => {
+      setWidgets(widgets);
+    })
+  }, []);
   return (
-    <div>
+    <BrowserRouter>
       <header>
         <h1>Header</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          {/* todo - dynamic link */}
+          {
+            widgets.map((widget) => <Link key={widget.path} to={widget.path}><p>{widget.name}</p></Link>)
+          }
+          <Link to="/about">About</Link>
+        </nav>
       </header>
 
-      <div>
-        todo - content - widget - router
-      </div>
+      <Suspense fallback={<div>loading...</div>}>
+          <Routes>
+            <Route path='/' component={Dashboard}></Route>
+            {/* todo - dynamic routes */}
+            {
+              widgets.map((widget) => (
+                <Route
+                  key={widget.path}
+                  path={widget.path}
+                  component={lazy(() => import(widget.file))}
+                  />
+              ))
+            }
+            <Route path='/about' component={About}></Route>
+          </Routes>
+      </Suspense>
 
       <footer>
         <h1>Footer</h1>
       </footer>
-    </div>
+    </BrowserRouter>
   )
 }
