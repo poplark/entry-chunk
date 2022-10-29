35c6a97d3f8ca423c5ca5587c52283a0127603d7
poplark <songkang666@126.com>
2022-10-30 01:00:59 +0800

    设置 chunkLoadingGlobal 为 webpackJsonp 为 lazy load widget 做准备

--- entry/webpack.config.js
+++ entry/webpack.config.js
@@ -7,14 +7,21 @@ console.log('base dir', baseDir);
 
 module.exports = {
   mode: 'development',
+  devtool: 'cheap-source-map',
   entry: {
     main: path.resolve(baseDir, 'entry/src/index.js')
   },
   output: {
-    path: path.resolve(baseDir, 'dist/entry')
+    path: path.resolve(baseDir, 'dist/entry'),
+    chunkLoadingGlobal: 'webpackJsonp'
   },
   resolve: {
     extensions: ['.js', '.jsx']
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+    'react-router-dom': 'ReactRouterDOM',
   },
   module: {
     rules: [
@@ -31,5 +38,32 @@ module.exports = {
       // filename: 'xx.html',
       // chunks: ['vendor', 'common', 'main']
     })
-  ]
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      /*  initial - 入口 chunk，对于异步导入的文件不处理
+          async - 异步 chunk，只对异步导入的文件处理
+          all - 全部 chunk
+      */
+      // 缓存分组
+      cacheGroups: {
+        // 第三方模块
+        vendor: {
+            name: 'vendor', // chunk 名称
+            priority: 1, // 权限更高，优先抽离，重要！！！！
+            test: /node_modules/,
+            minSize: 0, // 大小限制，太小的也没必要抽出来，可以设成 3KB, 5KB 之类的
+            minChunks: 1, // 最少复用过几次
+        },
+        // 公共模块
+        common: {
+            name: 'common',
+            priority: 0,
+            minSize: 0,
+            minChunks: 2,
+        }
+      }
+    }
+  }
 }
