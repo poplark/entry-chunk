97b55b26ba8e241df77eb8501265e99268736729
poplark <songkang666@126.com>
2022-11-03 15:24:31 +0800

    重构 plugin

--- extension/entry-chunk-plugin.js
+++ extension/entry-chunk-plugin.js
@@ -17,98 +17,125 @@ function isSourceMap(input) {
   );
 }
 
-function getPrefix(chunkId) {
-  return [
-    `(self["webpackJsonp"] = self["webpackJsonp"] || []).push([`,
-    `  ["${chunkId}"],`,
-    `  {`,
-    `    "${chunkId}": (`,
-    `      (__unused_webpack_module, __webpack_exports__, __webpack_require__) => {`,
-    `        __webpack_require__.r(__webpack_exports__);`,
-    ``,
-  ].join('\n');
-}
-function getPostfix(library) {
-  return [
-    ``,
-    `        __webpack_require__.d(__webpack_exports__, {`,
-    `          "default": () => ${library}.default`,
-    `        });`,
-    `      }`,
-    `    )`,
-    `  }`,
-    `]);`
-  ].join('\n');
-}
-
-function EntryChunkPlugin(options = {}) {
-  const { publicPath = '' } = options;
-
-  this.options = {
-    publicPath,
-  };
-}
-
 const PLUGIN_NAME = 'EntryChunkPlugin';
 const SUPPORTED_LIBRARY_TYPE = ['var', 'umd', 'umd2'];
 
-EntryChunkPlugin.prototype.apply = function (compiler) {
+class EntryChunkPlugin {
+  constructor(options = {}) {
+    const { publicPath = '' } = options;
+
+    this.options = {
+      publicPath,
+    };
+  }
+
+  apply(compiler) {
+    if (compiler.hooks) {
+      const plugin = { name: PLUGIN_NAME };
+      compiler.hooks.compilation.tap(plugin, compilation => {
+        compilation.hooks.additionalAssets.tapAsync(plugin, callback => {
+          this._addAffixes(compilation, callback);
+        });
+      });
+    } else {
+      compiler.plugin('compilation', compilation => {
+        compilation.plugin('additional-assets', callback => {
+          this._addAffixes(compilation, callback);
+        });
+      });
+    }
+  }
+
   /**
    * 将 entry 对应的 asset 添加 webpackJsonp 前缀，使其被载后，自动 push 到父级 modules 中进行缓存
    * @param {*} compilation 
    * @param {*} callback 
    * @returns 
    */
-  function chunkFn(compilation, callback) {
-    const { type, name: library } = compilation.outputOptions.library;
-    if (!SUPPORTED_LIBRARY_TYPE.includes(type)) {
-      const err = new Error(`The output.library.type of configuration of webpack must be one of ${SUPPORTED_LIBRARY_TYPE} when using ${PLUGIN_NAME}`);
-      compilation.errors.push(err);
-      return callback(err);
+  _addAffixes(compilation, callback) {
+    if (!this.isSupportedOptions(compilation)) {
+      return callback(new Error(`output.library is not supported with ${PLUGIN_NAME}`));
     }
-    // todo - 检查 library.name 的格式，不支持时提示
-    // 如果 filename 以 [name] [contenthash] [hash] 等等命名的，需要从 chunk 中找出 entry 的 asset 的文件名
-    for (const chunk of compilation.chunks) {
-      // todo - 不确定老版本，2,3,4 有 chunkGraph 概念，待验证
-      if (compilation.chunkGraph.getNumberOfEntryModules(chunk) > 0) {
-        for (const filename of chunk.files) {
-          // entry chunk 中添加全局 webpackJsonp 前缀
-          let chunkId = path.join(this.options.publicPath, filename);
-          const asset = compilation.assets[filename];
-          try {
-            const { source, map } = asset.sourceAndMap();
-
-            let replaceSource = new ReplaceSource(asset);
-            const length = source.length;
-            replaceSource.insert(length, getPostfix(library));
-            replaceSource.insert(0, getPrefix(chunkId));
-
-            compilation.assets[filename] = replaceSource;
-          } catch (error) {
-            compilation.errors.push(new Error(`${filename} from chunk loader plugin`));
-          }
-        }
+    for (const chunk of this.getEntryChunks(compilation)) {
+      for (const filename of chunk.files) {
+        this.addAffixes(compilation, filename);
       }
     }
     callback && callback();
   }
 
-  if (compiler.hooks) {
-    const plugin = { name: PLUGIN_NAME };
-    compiler.hooks.compilation.tap(plugin, compilation => {
-      compilation.hooks.additionalAssets.tapAsync(plugin, callback => {
-        chunkFn.call(this, compilation, callback);
-      });
-    });
-  } else {
-    compiler.plugin('compilation', compilation => {
-      compilation.plugin('additional-assets', callback => {
-        chunkFn.call(this, compilation, callback);
-      });
-    });
-  }
-};
+  /**
+   * 向 entry chunk file 中添加 webpackJsonp 前缀和后缀
+   * @param {t} compilation 
+   * @param {*} filename 
+   */
+  addAffixes(compilation, filename) {
+    const { name: library } = compilation.outputOptions.library;
+    let chunkId = path.join(this.options.publicPath, filename);
+    const asset = compilation.assets[filename];
+    try {
+      const { source, map } = asset.sourceAndMap();
 
-EntryChunkPlugin.loader = require.resolve('./entry-chunk-loader')
+      let replaceSource = new ReplaceSource(asset);
+      const length = source.length;
+      replaceSource.insert(length, this.getPostfix(library));
+      replaceSource.insert(0, this.getPrefix(chunkId));
+
+      compilation.assets[filename] = replaceSource;
+    } catch (error) {
+      compilation.errors.push(new Error(`${filename} from chunk loader plugin`));
+    }
+  }
+
+  getPrefix(chunkId) {
+    return [
+      `(self["webpackJsonp"] = self["webpackJsonp"] || []).push([`,
+      `  ["${chunkId}"],`,
+      `  {`,
+      `    "${chunkId}": (`,
+      `      (__unused_webpack_module, __webpack_exports__, __webpack_require__) => {`,
+      `        __webpack_require__.r(__webpack_exports__);`,
+      ``,
+    ].join('\n');
+  }
+
+  getPostfix(library) {
+    return [
+      ``,
+      `        __webpack_require__.d(__webpack_exports__, {`,
+      `          "default": () => ${library}.default`,
+      `        });`,
+      `      }`,
+      `    )`,
+      `  }`,
+      `]);`
+    ].join('\n');
+  }
+
+  isSupportedOptions(compilation) {
+    const { type, name } = compilation.outputOptions.library;
+    if (!SUPPORTED_LIBRARY_TYPE.includes(type)) {
+      const err = new Error(`The output.library.type of configuration of webpack must be one of ${SUPPORTED_LIBRARY_TYPE} when using ${PLUGIN_NAME}`);
+      compilation.errors.push(err);
+      return false;
+    }
+    // todo - 检查 library.name 的格式，不支持时提示
+    return true;
+  }
+
+  getEntryChunks(compilation) {
+    const chunks = [];
+    // 如果 filename 以 [name] [contenthash] [hash] 等等命名的，需要从 chunk 中找出 entry 的 asset 的文件名
+    for (const chunk of compilation.chunks) {
+      // todo - 不确定老版本，2,3,4 有 chunkGraph 概念，待验证
+      if (compilation.chunkGraph.getNumberOfEntryModules(chunk) > 0) {
+        chunks.push(chunk);
+      }
+    }
+    return chunks;
+  }
+}
+
+EntryChunkPlugin.loader = require.resolve('./entry-chunk-loader');
 
 module.exports = EntryChunkPlugin;
--- scripts/widget.js
+++ scripts/widget.js
@@ -41,18 +41,19 @@ function replaceEntry(config, widgetName) {
       break;
     case '[object Object]':
       const entries = Object.entries(config.entry);
-      if (entries.length === 0) {
-        const [_, mainEntry] = entries[0]; // todo - 只获取第一个么？？？
+      if (entries.length > 0) {
+        let [_, mainEntry] = entries[0]; // todo - 只获取第一个么？？？
         config.entry = {
           [widgetName]: mainEntry
         };
-      } else {
         // todo - 不应该支持多 entry ？？
-        config.entry = {};
-        for (const i in entries) {
-          const [_, mainEntry] = entries[i];
+        const len = entries.length;
+        for (let i=1; i<len; i++) {
+          [_, mainEntry] = entries[i];
           config.entry[`${widgetName}-${i}`] = mainEntry;
         }
+      } else {
+        console.warn('No correct entry in configuration of webpack!');
       }
       break;
     case '[object Function]':
