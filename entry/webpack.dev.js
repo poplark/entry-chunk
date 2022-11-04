7b7ac03288e11f99946a65e5e9ebd3d40f5ecd90
poplark <songkang666@126.com>
2022-11-04 16:50:21 +0800

    开发模式下 entry 的 public path 只能设置为 '', dev server 开启 hot 后，会自动添加 HMR plugin

--- entry/webpack.dev.js
+++ entry/webpack.dev.js
@@ -8,6 +8,9 @@ console.log('base dir', baseDir);
 module.exports = {
   mode: 'development',
   devtool: 'cheap-module-source-map',
+  output: {
+    publicPath: '', // todo - 开发模式下，HMR 会覆盖掉 entry-chunk-loader 注入的方法，目前只能写死 publicPath 为 ''
+  },
   devServer: {
     static: {
       directory: path.join(__dirname, 'public'),
@@ -16,7 +19,4 @@ module.exports = {
     open: false,
     hot: true,
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
 }
