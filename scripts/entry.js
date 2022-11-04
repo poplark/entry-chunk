cfad6bfa20eba9873066271229f2300a85f843b7
poplark <songkang666@126.com>
2022-11-04 16:15:52 +0800

    重构 entry 的 build 脚本

--- scripts/entry.js
+++ scripts/entry.js
@@ -6,23 +6,27 @@ const config = merge(baseConfig, require('../entry/webpack.prod'));
 
 console.log(`entry 配置信息`, config);
 
-const compiler = webpack(config);
-compiler.run((err, stats) => {
-  if (err) {
-    console.warn('!!!!!! compile error !!!!!!');
-    console.error(err);
-    return;
-  }
-  const { errors } = stats;
-  if (errors) {
-    console.info(`entry 构建失败`);
-    console.errors(errors);
-    return;
-  }
-  console.info(`entry 构建完成`);
-  // console.info(stats.chunks);
+startEntry(config);
 
-  compiler.close((closeErr) => {
-    // ...
+function startEntry(config) {
+  const compiler = webpack(config);
+  compiler.run((err, stats) => {
+    if (err) {
+      console.warn('!!!!!! compile error !!!!!!');
+      console.error(err);
+      return;
+    }
+    const { errors } = stats;
+    if (errors) {
+      console.info(`entry 构建失败`);
+      console.errors(errors);
+      return;
+    }
+    console.info(`entry 构建完成`);
+    // console.info(stats.chunks);
+
+    compiler.close((closeErr) => {
+      // ...
+    });
   });
-})
+}
