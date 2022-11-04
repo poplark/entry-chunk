24720ae75403bf014759d88f899629dc52aab971
poplark <songkang666@126.com>
2022-11-04 16:14:05 +0800

    重构 widget 的 build 脚本

--- scripts/widget.js
+++ scripts/widget.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const webpack = require('webpack');
 const { merge } = require('webpack-merge');
-const baseConfig = require('../config/webpack.widget.base');
 const { replaceEntry } = require('./utils');
+const baseConfig = require('../config/webpack.widget.base');
+const baseDir = process.cwd();
 
 const widget = process.argv[2];
-
 console.log(`widget ${widget} 将被编译`);
 
 const config = replaceEntry(
@@ -13,6 +14,8 @@ const config = replaceEntry(
     require(`../widgets/${widget}/webpack.prod`),
     {
       output: {
+        path: path.resolve(baseDir, `dist/widget/${widget}`),
+        publicPath: `/widget/${widget}/`, // 可配置 CDN 地址
         chunkLoadingGlobal: `webpack${widget}Jsonp`, // 需要为每个 widget 指定不同的 webpackJsonp 名称，不然可能会导致 widget chunk 加载时的冲突
       }
     }
@@ -22,25 +25,29 @@ const config = replaceEntry(
 
 console.log(`widget ${widget} 配置信息`, config);
 
-const compiler = webpack(config);
-compiler.run((err, stats) => {
-  if (err) {
-    console.warn('!!!!!! compile error !!!!!!');
-    // console.error(err);
-    return;
-  }
-  const { errors } = stats;
-  if (errors) {
-    console.info(`widget ${widget} 构建失败`);
-    // console.errors(errors);
-    return;
-  }
-  console.info(`widget ${widget} 构建完成`);
-  // console.info(stats.chunks);
+startWidget(config);
 
-  // todo - 将生成的 assets 数据入库保存
+function startWidget(config) {
+  const compiler = webpack(config);
+  compiler.run((err, stats) => {
+    if (err) {
+      console.warn('!!!!!! compile error !!!!!!');
+      // console.error(err);
+      return;
+    }
+    const { errors } = stats;
+    if (errors) {
+      console.info(`widget ${widget} 构建失败`);
+      // console.errors(errors);
+      return;
+    }
+    console.info(`widget ${widget} 构建完成`);
+    // console.info(stats.chunks);
 
-  compiler.close((closeErr) => {
-    // ...
-  });
-})
+    // todo - 将生成的 assets 数据入库保存
+
+    compiler.close((closeErr) => {
+      // ...
+    });
+  })
+}
